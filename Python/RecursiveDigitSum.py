b62fc0a2e0197b3b00abbd648712821b783add23
charlestian23 <charles.tian23@gmail.com>
2023-11-14 13:05:41 -0500

    Update RecursiveDigitSum.py
    New solution that works for all test cases. Old solution had an error where converting from string to int failed since the string could be too long.

--- Python/RecursiveDigitSum.py
+++ Python/RecursiveDigitSum.py
@@ -1,19 +1,8 @@
 def superDigit(n, k):
-    p = int(n)
-    if p < 10:
-        return p
-    total = 0
-    while p != 0:
-        total += p % 10
-        p //= 10
-    return superDigitHelper(total * k)
+    if len(n) == 1 and k == 1:
+        return int(n)
 
-
-def superDigitHelper(p):
-    if p < 10:
-        return p
     total = 0
-    while p != 0:
-        total += p % 10
-        p //= 10
-    return superDigitHelper(total)
+    for num in n:
+        total += int(num)
+    return superDigit(str(total * k), 1)
